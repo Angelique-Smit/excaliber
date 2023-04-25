1d4275ecca571005221638ea3db27dfb8fbc82e3
Angelique-Smit <ajd.smit@gmail.com>
2023-04-25 15:48:51 +0200

    2nd commit best programmer ever uwu

--- src/js/game.js
+++ src/js/game.js
@@ -10,12 +10,41 @@ export class Game extends Engine {
     }
 
     startGame() {
-        console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(400, 300)
-        fish.vel = new Vector(-10,0)
-        this.add(fish)
+        let spongebob = new Actor()
+        spongebob.graphics.use(Resources.patrick.toSprite());
+        spongebob.scale = new Vector(2.5 , 2.5);
+        spongebob.pos = new Vector(400 , 300);
+        this.add(spongebob);
+
+        for (let i = 0; i < 40; i++) {
+        console.log(i)
+
+        let fish = new Actor()
+        fish.graphics.use(Resources.Fish.toSprite());
+        fish.pointer.useGraphicsBounds = true;
+        fish.enableCapturePointer = true;
+        fish.pos = new Vector(Math.random() * 800 , Math.random() * 600);
+        fish.vel = new Vector(Math.random() * 10 , Math.random() * 10);
+        this.add(fish);
+
+        let sus = new Actor()
+        sus.graphics.use(Resources.amongUs.toSprite());
+        sus.pointer.useGraphicsBounds = true;
+        sus.enableCapturePointer = true;
+        sus.scale = new Vector(0.1 , 0.1);
+        sus.pos = new Vector(Math.random() * 800 , Math.random() * 600);
+        sus.vel = new Vector(Math.random() * 10 , Math.random() * 10);
+        this.add(sus);
+
+        fish.on("pointerup", () => {
+            fish.kill();
+        })
+
+        sus.on("pointerup", () => {
+            sus.kill();
+        })
+
+        }  
     }
 }
 
--- src/js/resources.js
+++ src/js/resources.js
@@ -1,9 +1,13 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import fishImage from '../images/fish.png'
+import sus from '../images/among-us-sus.gif'
+import spongebob from '../images/patrick.png'
 
 const Resources = {
-    Fish: new ImageSource(fishImage)
+    Fish: new ImageSource(fishImage),
+    amongUs: new ImageSource(sus),
+    patrick: new ImageSource(spongebob)
 }
-const ResourceLoader = new Loader([Resources.Fish])
+const ResourceLoader = new Loader([Resources.Fish, Resources.amongUs, Resources.patrick])
 
 export { Resources, ResourceLoader }
